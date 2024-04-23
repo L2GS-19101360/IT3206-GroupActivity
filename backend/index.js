f1f9a60c822b0c17d821f335395f713d9aa09be0
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-04-23 20:53:07 +0800

    Frontend and Backend Created

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const cors = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3306
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello World 🙂");
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
