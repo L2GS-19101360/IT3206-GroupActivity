941bc8a702bae0985fb1fdd85a69627b4b93ce10
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-04-24 15:36:49 +0800

    Database Created

---
+++ it3206-taskmanager.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2024 at 09:36 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `it3206-taskmanager`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `status` enum('PENDING','ONGOING','COMPLETED') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `title`, `description`, `status`) VALUES
+(1, 'Wash Clothes', 'Sort laundry, start the washing machine, and hang clothes to dry.', 'PENDING'),
+(2, 'Buy Groceries', 'Make a list of essential items, visit the grocery store, and purchase fruits, vegetables, and other household necessities.', 'PENDING'),
+(3, 'Clean the House', 'Vacuum floors, dust surfaces, and clean bathrooms to maintain a tidy living space.', 'PENDING'),
+(4, 'Pay Bills', 'Review utility bills and other expenses, and make payments online or by mail to stay on top of finances.', 'PENDING'),
+(5, 'Exercise', 'Go for a run, attend a yoga class, or do a workout at home to stay active and healthy.', 'PENDING');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
