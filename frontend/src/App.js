93244fe10d6a759f6af8057c4b022be62fdb1b74
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-05-04 02:48:04 +0800

    All Merge Conflict is fixed. Going to push and merge to main. Then updates everyone's branches

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,17 +12,9 @@ import { DataGrid } from '@mui/x-data-grid';
 function App() {
   return (
 
-    <div className="App">
-
-      <Tasks />
-    </div>
-  );
-
     <>
       <NavigationBar />
-      <div className='main-container'>
-
-      </div>
+      <Tasks />
     </>
   )
 
