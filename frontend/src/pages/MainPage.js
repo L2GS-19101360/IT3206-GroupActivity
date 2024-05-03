a480bd4bcc797a2ac01dc0d1c15b6b2058cabf94
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-05-03 15:13:19 +0800

    Able to fetch and display the data into the page

--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import Button from '@mui/material/Button';
-
+import axios from 'axios'
 import TableRow from '@mui/material/TableRow';
 
 
-export default function Tasks(){
+export default function Tasks() {
+
+    const [taskArray, settaskArray] = useState([]);
+
+    useEffect(() => {
+        const fetchTask = async () => {
+            try {
+                const response = await axios.get('http://localhost:8080/api/tasks/');
+                settaskArray(response.data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchTask();
+    }, []);
+
+    console.log(taskArray)
 
     const columns = [
-        { 
-            id: 'title', 
+        {
+            id: 'title',
             label: 'Title',
         },
         {
-          id: 'description',
-          label: 'Description',
+            id: 'description',
+            label: 'Description',
         },
         {
             id: 'progress',
@@ -31,23 +48,15 @@ export default function Tasks(){
         }
     ];
 
-    const dummyData = [
-        {title: 'Integ Prog Finals', desc: 'Make Final project', status: 'Ongoing'},
-        {title: 'SQA Finals', desc: 'Make Final project', status: 'Pending'},
-        {title: 'Capstone Defense', desc: 'Finalize ppt and practice for defense', status: 'Ongoing'},
-        {title: 'PE Finals', desc: 'Sportsfest on May 19', status: 'Pending'},
-        {title: 'Arts Project', desc: 'Make art booklet', status: 'Ongoing'}
-    ]
-
-    return(
+    return (
         <>
 
             <button>Create Contact</button>
-            <form class="example" action="/action_page.php">
+            <form className="example" action="/action_page.php">
                 <input type="text" placeholder="Enter Contact" name="search"></input>
-                 <button type="submit">Hello</button>
+                <button type="submit">Hello</button>
             </form>
-      
+
             <TableContainer sx={{ padding: 10 }}>
                 <Table stickyHeader aria-label="sticky table">
                     <TableHead>
@@ -60,14 +69,12 @@ export default function Tasks(){
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {dummyData.map((row) => (
-                            <TableRow>
-                                <TableCell>{row.title}</TableCell>
-                                <TableCell>{row.desc}</TableCell>
-                                <TableCell>{row.status}</TableCell>
-
-                                <TableCell><Button variant='contained'>Info</Button></TableCell>
-
+                        {taskArray.map((task) => (
+                            <TableRow key={task.id}>
+                                <TableCell style={{ maxWidth: "50px" }}>{task.title}</TableCell>
+                                <TableCell style={{ wordWrap: "break-word", wordBreak: "break-word", maxWidth: "150px" }}>{task.description}</TableCell>
+                                <TableCell style={{ wordWrap: "break-word", wordBreak: "break-word", maxWidth: "1px" }}>{task.status}</TableCell>
+                                <TableCell style={{ maxWidth: "10px" }}><Button variant='contained'>Info</Button></TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
