fb04341a0e0a63afa71b7877346a553b95bcfe74
nichoblina <nichoblina@gmail.com>
2024-05-04 22:17:49 +0800

    Navigation bar and action bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,14 @@
-
-import logo from './logo.svg';
 import './App.css';
-import Tasks from './pages/MainPage';
-
+import PageContent from './pages/PageContent';
 import './App.css';
 import NavigationBar from './components/Navbar'
 import { DataGrid } from '@mui/x-data-grid';
 
-
-
 function App() {
   return (
-
     <>
       <NavigationBar />
-      <Tasks />
+      <PageContent />
     </>
   )
 
