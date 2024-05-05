43cbc3e63cf7baca01c58d7f62ef48d78dad71a7
Lorenz Gil Suico <19101360@usc.edu.ph>
2024-05-06 00:41:01 +0800

    Updated Frontend Display

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -4,11 +4,14 @@ import Header from './Header';
 import ActionBar from './ActionBar';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
+import { TextField } from "@mui/material";
+import Button from '@mui/material/Button';
 
 export default function TaskList() {
   const [taskArray, setTaskArray] = useState([]);
   const [selected, setSelected] = useState(null);
   const [rerender, setRerender] = useState(false);
+  const [searchTerm, setSearchTerm] = React.useState("");
 
   const fetchTasks = async () => {
     try {
@@ -23,13 +26,34 @@ export default function TaskList() {
     fetchTasks();
   }, [rerender]);
 
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredTasks = taskArray.filter((task) => task.title.toLowerCase().includes(searchTerm.toLowerCase()));
+
   return (
     <>
       <div className='main-container'>
         <Header />
+
+        <div className="search-bar">
+          <TextField
+            id="standard-basic"
+            label="Search Task"
+            variant="standard"
+            value={searchTerm}
+            onChange={handleSearch}
+            style={{ width: "1000px" }}
+          />
+          {/* <Button variant="contained" style={{ backgroundColor: "#015901" }} onClick={() => handleSearch()}>
+            Search
+          </Button> */}
+        </div>
+
         <ActionBar setTaskArray={setTaskArray} setRerender={setRerender} />
         <TaskTable
-          tasks={taskArray}
+          tasks={filteredTasks}
           selected={selected}
           setSelected={setSelected}
           setRerender={setRerender}
@@ -43,25 +67,62 @@ function TaskTable({ tasks, selected, setSelected, setRerender }) {
   const columns = [
     {
       field: 'title',
-      headerName: 'Task Name',
+      headerName: 'Task Title',
       minWidth: 400,
       headerAlign: 'left',
       fontWeight: 'bold',
     },
     {
       field: 'description',
-      headerName: 'Description',
+      headerName: 'Task Description',
       flex: 1,
+      minWidth: 510,
       headerAlign: 'center',
       fontWeight: 'bold',
     },
     {
-      field: 'progress',
-      headerName: 'Progress',
+      field: 'status',
+      headerName: 'Task Status',
       minWidth: 400,
-      headerAlign: 'right',
+      headerAlign: 'left',
       fontWeight: 'bold',
     },
+    {
+      field: 'action',
+      headerName: 'Task Action',
+      minWidth: 394,
+      headerAlign: 'left',
+      fontWeight: 'bold',
+      renderCell: (params) => {
+        const task = params.row;
+
+        return (
+          <div>
+            {task.status === 'PENDING' ? (
+              <Button
+                variant='contained'
+                color='success'
+                onClick={() => startTask(task.id)}
+              >
+                Start Task
+              </Button>
+            ) : task.status === 'ONGOING' ? (
+              <Button
+                variant='contained'
+                color='error'
+                onClick={() => endTask(task.id)}
+              >
+                End Task
+              </Button>
+            ) : (
+              <Button variant='contained' disabled>
+                Completed
+              </Button>
+            )}
+          </div>
+        );
+      }
+    }
   ];
 
   const deleteTask = (taskId) => {
@@ -101,7 +162,7 @@ function TaskTable({ tasks, selected, setSelected, setRerender }) {
 
   return (
     <Box
-      sx={{ height: 400, width: '100%', display: 'flex', marginTop: '10px' }}
+      sx={{ height: 500, width: '100%', display: 'flex', marginTop: '10px' }}
     >
       <DataGrid
         rows={tasks}
